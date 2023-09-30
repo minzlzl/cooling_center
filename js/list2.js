fdf32556a9ffe72fe217191c8ae899edbf2d9307
minzlzl <133857306+minzlzl@users.noreply.github.com>
2023-09-30 15:13:58 +0900

    230930

---
+++ js/list2.js
@@ -0,0 +1,18 @@
+const elList = document.querySelector('.list_con');
+
+fetch('./js/db/data.json')
+.then(res=>{
+  return res.json();
+})
+.then(data=>{
+  // console.log(data.DATA);
+  data.DATA.forEach(function(v){
+    elList.innerHTML = `
+                            <ul><li>${v.r_area_nm}</li>
+                            <li>${v.r_detl_add}</li>
+                            <li>${v.dt_start}</li>
+                            <li>${v.dt_end}</li></ul>
+                            `
+  });
+});
+
